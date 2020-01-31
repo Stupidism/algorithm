c102c3c5dd6a577da0680b9669ea6e06b42f5d94
Stupidism <5801015+Stupidism@users.noreply.github.com>
2020-01-31 13:36:03 +0800

    Update 159.longest-substring-with-at-most-two-distinct-characters.js

--- 159.longest-substring-with-at-most-two-distinct-characters.js
+++ 159.longest-substring-with-at-most-two-distinct-characters.js
@@ -5,29 +5,27 @@ const assert = require('assert');
  * @return {number}
  */
 var lengthOfLongestSubstringTwoDistinct = function(s) {
-  let maxLen = 0;
+  let maxLen = Math.min(s.length, 2);
   let start = -1;
   const charPositions = new Map();
 
-  for (let i = 0; i < s.length; i++) {
-    const c = s[i];
+  let end;
+  for (end = 0; end < s.length; end++) {
+    const c = s[end];
     const charPosition = charPositions.get(c);
+    // console.log('charPosition', c, start, charPosition, charPositions);
 
     if (charPosition == null && charPositions.size === 2) {
-      let charToRemove;
-      for (const [char] of charPositions) {
-        if (char !== s[i - 1]) {
-          charToRemove = char;
-        }
-      };
-      start = charPositions.get(charToRemove);
-      charPositions.delete(charToRemove);
-    } else {
-      maxLen = Math.max(maxLen, i - start);
+      maxLen = Math.max(maxLen, end - start - 1);
+      start = Math.min(...charPositions.values());
+      // console.log('charToRemove', start, s[start]);
+      charPositions.delete(s[start]);
     }
 
-    charPositions.set(c, i);
+    charPositions.set(c, end);
   }
+  maxLen = Math.max(maxLen, end - start - 1);
+
   return maxLen;
 };
 
