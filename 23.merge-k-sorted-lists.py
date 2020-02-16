1153e045ccd6a776979289ac4938fd225093c979
stupidism <sunfeng32@qq.com>
2020-02-16 22:50:30 +0800

    Update 23.merge-k-sorted-lists.py

---
+++ 23.merge-k-sorted-lists.py
@@ -0,0 +1,34 @@
+from typing import List
+import heapq
+
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+  def __lt__(self, other):
+    return self.val < other.val
+
+class Solution:
+  def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+    head = ListNode(None)
+    tail = head
+    queue = []
+    for index, lst in enumerate(lists):
+      heapq.heappush(queue, (lst.val, index))
+
+    while(len(queue)):
+      index = heapq.heappop(queue)[1]
+      lst = lists[index]
+      lists[index] = lst.next
+      tail.next = lst
+      tail = lst
+      if (lst.next is not None):
+        heapq.heappush(queue, (lst.next.val, index))
+
+    return head.next
+
+lists = list(map(ListNode, [3,2,1,5,4]))
+lists[0].next = ListNode(3)
+lists[2].next = ListNode(6)
+Solution().mergeKLists(lists)
