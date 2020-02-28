156a199dab3f2cacb7ce068771be6720e2913478
stupidism <sunfeng32@qq.com>
2020-02-28 23:15:29 +0800

    Update leetcode/21.merge-two-sorted-lists.js

--- leetcode/21.merge-two-sorted-lists.js
+++ leetcode/21.merge-two-sorted-lists.js
@@ -10,7 +10,33 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
 var mergeTwoLists = function(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  const prehead = new ListNode(null);
+  prehead.next = l1;
+
+  let sortedNode = prehead;
+  let node = l2;
+  while(node && sortedNode) {
+    const next = sortedNode.next;
+    if (!next) {
+      sortedNode.next = node;
+      break;
+    }
+    if (node.val < next.val) {
+      sortedNode.next = node;
+      node = next;
+    }
+    sortedNode = sortedNode.next;
+  }
+   
+  return prehead.next;
+};
+ 
+var mergeTwoLists1 = function(l1, l2) {
   if (l1 == null) return l2;
   if (l2 == null) return l1;
   if(l1.val <= l2.val) {
